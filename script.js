c8c19d6f0ca25eed520a64ab313ee3d29362cf31
zafyyre <zaaf17@hotmail.com>
2021-11-28 13:15:13 -0800

    Third function for script.js
    This function adds three contacts on the index page

--- script.js
+++ script.js
@@ -27,3 +27,40 @@ function createSingleIndex(contact) {
     return a
 
 }
+
+let contactList = [{
+        name: "Roberta Dobbs",
+        phone: "778-555-1234",
+        address: "101 Main St, Anytown, USA",
+        email: "subgenius@slack.example.com",
+    },
+    {
+        name: "Bugs Bunny",
+        phone: "123-867-5309",
+        address: "Warner Brothers Animation Lot",
+        email: "whatsup@doc.example.com",
+    },
+    {
+        name: "Zaafir Ali",
+        phone: "911",
+        address: "The dumpster",
+        email: "waaf21@hotmail.com"
+    }
+]
+
+function renderIndex(contactList) {
+    for (let i = 0; i < contactList.length; i++) {
+        let a = document.createElement('a');
+        a.setAttribute('href', 'page3.html');
+
+        let div = document.createElement('div');
+        div.setAttribute('class', 'contact');
+        a.appendChild(div);
+
+        let p = document.createElement('p')
+        div.appendChild(p)
+        p.textContent = `${contactList[i]["name"]}`;
+        let main = document.querySelector(".main");
+        main.appendChild(a)
+    }
+}
