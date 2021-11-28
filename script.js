7cafc6fdef0ec0f167811dec858ef78a83b1bd6b
zafyyre <zaaf17@hotmail.com>
2021-11-28 13:02:42 -0800

    Second Function for script.js
    Second Function createSingleIndex(contact)

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function cleanupindex() {
         main.removeChild(main.firstChild);
     }
 }
+
+const contact = {
+    name: "Roberta Dobbs",
+    phone: "778-555-1234",
+    address: "101 Main St, Anytown, USA",
+    email: "subgenius@slack.example.com",
+}
+
+function createSingleIndex(contact) {
+
+    let a = document.createElement('a');
+    a.setAttribute('href', 'page3.html');
+
+    let div = document.createElement('div');
+    div.setAttribute('class', 'contact');
+    a.appendChild(div);
+
+    let p = document.createElement('p')
+    div.appendChild(p);
+    p.textContent = `${contact["name"]}`;
+    return a
+
+}
