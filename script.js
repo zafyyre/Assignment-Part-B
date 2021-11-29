7f9ae2887b810f3b70a082d74dc32ce9501f0dcc
zafyyre <zaaf17@hotmail.com>
2021-11-29 00:29:51 -0800

    Fifth Function for script.js
    This function adds a contacts characteristics on the view page

--- script.js
+++ script.js
@@ -71,3 +71,55 @@ function cleanUpView() {
         main.removeChild(main.firstChild);
     }
 }
+
+function renderView(contactList) {
+    let img = document.createElement('img');
+    img.setAttribute('src', './img/profile.jpg');
+    img.setAttribute('class', 'profilepic');
+    img.setAttribute('alt', 'Profile picture');
+
+    let contactname = document.createElement('div');
+    contactname.setAttribute('class', 'contactname');
+    contactname.textContent = `name: ${contactList[1]["name"]}`;
+    contactname.appendChild(img);
+
+    let contactinfo = document.createElement('div');
+    contactinfo.setAttribute('class', 'contactinfo');
+    contactinfo.appendChild(contactname);
+
+    let contactemail = document.createElement('div');
+    contactemail.setAttribute('class', 'contactemail');
+    contactemail.textContent = `email: ${contactList[1]["email"]}`;
+    contactinfo.appendChild(contactemail);
+
+    let contactphone = document.createElement('div');
+    contactphone.setAttribute('class', 'contactphone');
+    contactphone.textContent = `phone: ${contactList[1]["phone"]}`;
+    contactinfo.appendChild(contactphone);
+
+    let contactaddress = document.createElement('div');
+    contactaddress.setAttribute('class', 'contactaddress');
+    contactaddress.textContent = `address: ${contactList[1]["address"]}`;
+    contactinfo.appendChild(contactaddress);
+
+    let buttons = document.createElement('div');
+    buttons.setAttribute('class', 'buttons');
+    contactinfo.appendChild(buttons)
+
+    let buttonedit = document.createElement('button');
+    buttonedit.setAttribute('class', 'button edit');
+    buttonedit.setAttribute('value', 'Edit')
+    buttonedit.textContent = 'Edit';
+    buttons.appendChild(buttonedit);
+
+    let buttonclose = document.createElement('button');
+    buttonclose.setAttribute('class', 'button close');
+    buttonclose.setAttribute('value', 'Close');
+    buttonclose.textContent = 'Close';
+    buttons.appendChild(buttonclose)
+
+    let main = document.createElement('div');
+    main.setAttribute('class', 'main');
+    main.appendChild(contactinfo)
+    document.body.appendChild(main)
+}
